fe891c93b3e481c1bb84724b786b74592621b74c
GabrielSiqueiraa <113144528+GabrielSiqueiraa@users.noreply.github.com>
2023-09-22 16:27:29 -0300

    Update index.js

--- express-01/src/models/index.js
+++ express-01/src/models/index.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 import getUserModel from "./user";
 import getMessageModel from "./message";
 
-const sequelize = new Sequelize(process.env.MY_SECRET, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectModule: require('pg')
 });
 
